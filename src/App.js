e50d5c69b0dada599b74b99f5814166b755c5cff
alvinmithamo <ngangaalvin100@gmail.com>
2024-07-24 20:15:07 +0300

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import ClientDashboard from './pages/ClientDashboard';
+import AdminDashboard from './pages/AdminDashboard';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Navbar from './components/Navbar';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact Component={Home} />
+        <Route path='/client-dashboard' Component={ClientDashboard} />
+        <Route path='/admin-dashboard' exact Component={AdminDashboard} />
+        <Route path='/login' exact Component={Login} />
+        <Route path='/signup' exact Component={Signup} />
+      </Switch>
+    </Router>
+  )
+
 }
 
 export default App;
