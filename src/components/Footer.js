92c9613fefc9dd80789d0b72ea057ff9d7a5985c
alvinmithamo <ngangaalvin100@gmail.com>
2024-08-02 17:33:13 +0300

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p className='footer-main'>&copy; 2024 BOOKRAHISI. All rights reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
