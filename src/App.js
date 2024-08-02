4502ec60ee1ea1794a6ed1179a6c182d099c030c
alvinmithamo <ngangaalvin100@gmail.com>
2024-08-02 17:31:05 +0300

    imported components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './AuthContext';
 import Home from './pages/Home';
 import ClientDashboard from './pages/ClientDashboard';
 import AdminDashboard from './pages/AdminDashboard';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Navbar from './components/Navbar';
-import logo from './logo.svg';
+import Footer from './components/Footer';
+import Profile from './pages/Profile';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <AuthProvider>
     <Router>
       <Navbar />
-      <Switch>
+      <Routes>
         <Route path='/' exact Component={Home} />
         <Route path='/client-dashboard' Component={ClientDashboard} />
         <Route path='/admin-dashboard' exact Component={AdminDashboard} />
         <Route path='/login' exact Component={Login} />
         <Route path='/signup' exact Component={Signup} />
-      </Switch>
+        <Route path='/profile' exact Component={Profile} />
+      </Routes>
+      <Footer />
     </Router>
+    </AuthProvider>
   )
 
 }
